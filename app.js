eaa1bbb205c8695595bd9a0795657ac2af0079ff
James Burnside <jcburnside@gmail.com>
2017-07-05 20:07:32 -0400

    INIT

---
+++ app.js
@@ -0,0 +1,9 @@
+(()=>{
+	var app=angular.module('workoutlog',['ui.router']);
+	function config($urlRouterProvider){
+		$urlRouterProvider.otherwise('/signin');
+	}
+	config.$inject=['$urlRouterProvider'];
+	app.config(config);
+	app.constant('API_BASE','//localhost:3000/api');
+})()
